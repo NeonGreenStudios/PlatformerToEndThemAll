bbfc3d9b8815e24c8d589f824d6a023fc4579b8d
extremespancake <extremespancake@googlemail.com>
2014-03-14 18:38:00 +0000

    Revert "TBP is no loger on the branch"
    This reverts commit 960a4349f6a96e7f8557007037efb0520b264267.

---
+++ Tile Based Platformer Test 2/Tile Based Platformer Test 2/Tile Based Platformer Test 2/Classes/Game1.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Tile_Based_Platformer_Test_2
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        public Level Current_Level = new Level();
+        public Player Player1 = new Player();
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = 1280; //horizontal resolution
+            graphics.PreferredBackBufferHeight = 720; //vertical resolution
+            graphics.IsFullScreen = false; //fullscreen mode
+            graphics.ApplyChanges(); //sets graphics using above settings
+            IsMouseVisible = true; //shows mouse in window
+            this.Window.Title = "Tile Engine V1"; //window title
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Current_Level.LoadFile("Content/Maps/Chris game.txt", Content);
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
+                Current_Level.Y_Axis = 0;
+                Current_Level.X_Axis = 0;
+                Player1.Draw(spriteBatch, Content);
+                Current_Level.Draw(spriteBatch, Content);
+                                          
+            // TODO: Add your drawing code here
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
