71763c77cccae99e5f53294cda7152401db24b5f
Shiza-Kicks <samwikwiki@hotmail.co.uk>
2014-03-14 17:28:49 +0000

    Tile Loader thing
    Loads in files and displays them

---
+++ Tile Based Platformer Test 2/Tile Based Platformer Test 2/Tile Based Platformer Test 2/Classes/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tile_Based_Platformer_Test_2
+{
+    public class Player : Sprite
+    {
+
+        public void Keyboard()
+        {
+
+        }
+    }
+}
